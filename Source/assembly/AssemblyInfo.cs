5948e2fbb7523abdec04d94aa84685f19e4647d2
Majorvader111 <comeon92rules@gmail.com>
2021-01-24 13:54:03 +0800

    Add Complied Version With Butchers Tank Spam Fix

--- Source/assembly/AssemblyInfo.cs
+++ Source/assembly/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("TestFlightRF")]
+[assembly: AssemblyTitle("RealFuels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("john")]
+[assembly: AssemblyCopyright("NathanKell")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,9 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("12.6.0")]
+[assembly: AssemblyInformationalVersionAttribute("12.6.0")]
+[assembly: AssemblyFileVersion("12.6.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -25,5 +27,4 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-// Need RF to do this first
-// [assembly: KSPAssemblyDependency("RealFuels", 10, 8)]
+[assembly: KSPAssemblyDependency("SolverEngines", 3, 8)]
